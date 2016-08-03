bafd100ad31e8330dc0157dc05201bb0e865a164
Akshay Verma <akshayverma295@gmail.com>
2016-08-04 00:18:34 +0530

    Minor Lint fixes

--- ckanext/budgetviz/theme/public/timeseries_ipfs.js
+++ ckanext/budgetviz/theme/public/timeseries_ipfs.js
@@ -1,7 +1,8 @@
 (function() {
+
     //d3.csv sends an HTTP GET request using resource_url provided from the template.
     d3.csv(resource_url, function(error, data) {
-       
+       console.log(data)
 
        
         mungeddata = mungeData(data)
